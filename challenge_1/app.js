5446652617ed7c65d7c40624edcc7a0452b124c9
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-02 16:42:43 -0800

    Have created a working board. Need to add winning criteria

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,69 @@
+const board = [
+              ['1','2','3'],
+              ['4','5','6'],
+              ['7','8','9']
+              ];
+var playerCounter = 0;
+var player = '';
+
+var findRow = function(id) {
+  console.log('find Row id: ', id)
+  var index = id;
+  if (index < 3) {
+      return board[0];
+  } else if (index < 6) {
+      return board[1];
+  } else {
+      return board[2];
+  }
+};
+
+var findIndex = function(id) {
+  console.log('findIndex id: ', id);
+  var index = id;
+  if (index<3) {
+      return index;
+  } else if (index < 6) {
+      return index-3;
+  } else {
+      return index-6;
+  }
+};
+
+function saveToBoard(playerCounter){
+  if( playerCounter%2 !== 0)
+  {
+       player = 'X';
+
+  }else{
+       player = 'O';
+  }
+  return player;
+}
 
 
+
+function placePiece() {
+  playerCounter++; // on all odd numbers X will be a player
+
+  var id =event.target.id -1;
+  var row = findRow(id);
+  var rowIndex = findIndex(id)
+  saveToBoard(playerCounter, row, rowIndex )
+
+  row[rowIndex] = player;
+  event.target.innerHTML = player;
+
+
+
+  console.log('row and index value: ', row[rowIndex])
+  console.log('row: ', row)
+  console.log('row index: ', rowIndex)
+  console.log('event.target.innerHTML: ',  event.target.innerHTML);
+
+
+}
+const block = document.getElementsByClassName('block');
+for(var i = 0; i < block.length;i++)
+{block[i].addEventListener(("click"), placePiece)
+};
