3bd93034b8415c3a7a1b224b8fde221ecb60963d
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-04 14:33:46 -0800

    Added state to Checkout button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,15 +1,25 @@
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+                text : "Checkout"
+        }
+        this.receiveForms = this.receiveForms.bind(this);
     }
 
+
+receiveForms(){
+    alert('Test worked')
+
+}
     render() {
-         return <button id="checkout">Checkout</button>
+    return (<button id="checkout" onClick={this.receiveForms}>{this.state.text}</button>
+         
         // <form id="formData" method="POST" action="/uploadjson">
         //             <input type="text" name="jsonText">
         //             <button>Submit</button>
         //         </form>;
-    }
+         )}
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
