f35b4d0195457f77ac0bbc823f653348e9775212
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-04 14:10:52 -0800

    Have server listening, babel is transpiling and saving files into public folder, working in making class components with state

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,15 @@
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
 
+    render() {
+         return <button id="checkout">Checkout</button>
+        // <form id="formData" method="POST" action="/uploadjson">
+        //             <input type="text" name="jsonText">
+        //             <button>Submit</button>
+        //         </form>;
+    }
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
