87a4abc77f52a8d9c8391b88209918f8a9c50f78
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-04 18:11:02 -0800

    Users can send a POST of the forms they send in, express is able to read the properites of req correctly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,10 +1,10 @@
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-                text : "Checkout",
-                value : "Account info",
-                page: "Checkout"
+            text: "Checkout",
+            value: "",
+            page: "Checkout"
         }
         this.receiveForms = this.receiveForms.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -12,57 +12,113 @@ class App extends React.Component {
     }
 
 
-receiveForms() {
-    this.setState({
-      page: 'Payment Processing --->'
-    })
-}
-handleChange(event) {
-        console.log('value: ',event.target.value)
-        console.log(event.target.value)
-        this.setState({value: event.target.value});
-      }
-distributeForm() {
-    event.preventDefault()
-    FormAccountInfo();
-    console.log(event.target)
-   
-    alert('this test works')
-}
+    receiveForms() {
+        event.preventDefault()
+    }
 
-FormAccountInfo = () => {
-return (<form>
-<input type="submit"></input>
-</form>)
-}
-FormProcessPayment = () => {
-<form>
-<input type="submit"></input>
-</form>
-}
-FormShippingInfo = () => {
-<form>
-<input type="submit"></input>
-</form>
-}
+
+    handleChange(event) {
+        event.preventDefault()
+        console.log('value: ', event.target.value)
+        console.log(event.target.value)
+        this.setState({ value: event.target.value });
+    }
+
+
+    distributeForm() {
+        event.preventDefault()
+        console.log(event.target)
+
+        alert('this test works')
+    }
+
 
     render() {
-    return (
-    <div> <form onSubmit={this.receiveForms}>
-            <label> {this.state.text}:
-                <select value={this.state.value} onChange={this.handleChange}>
-                    <option value="Process Payment">Process Payment</option>
-                    <option value="Account Info">Account Info</option>
-                    <option value="Shipping Info">Shipping Info</option>
-                </select>
-                <input type="submit" value="Submit" onClick={this.distributeForm}  />
-            </label>
-            
+
+        if (this.state.value === "Account Info") {
+            return <form id="accountForm" action="api/client" method="POST">
+                        <h1>Please enter your {this.state.value}</h1>
+                        <p>Name:</p>
+                <input
+                    type='text'
+                    name='name'
+                />
+                <p>Email</p>
+                <input
+                    type='text'
+                    name="email"
+                />
+                <p>Password</p>
+                <input
+                    type='text'
+                    name="password"
+                />
+                <button>onSubmit</button>
             </form>
-            
-    </div>
-            
-         )}
+        }else if (this.state.value === "Shipping Info") {
+    return <form id="shippingForm">
+                <h1>Please enter your {this.state.value}</h1>
+                <p>Address:</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <p>City:</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <p>State</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <p>Zipcode:</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <button>onSubmit</button>
+    </form>
+        }else if (this.state.value === "Process Payment") {
+    return <form id="processForm">
+                <h1>Please enter your {this.state.value}</h1>
+                <p>Name:</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <p>Email</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <p>Password</p>
+        <input
+            type='text'
+            onChange={this.handleChange}
+        />
+        <button>onSubmit</button>
+    </form>
+        }
+        else{
+        return (
+            <div> <form onSubmit={this.receiveForms}>
+                <label> {this.state.text}:
+                <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="Process Payment">Process Payment</option>
+                        <option value="Account Info">Account Info</option>
+                        <option value="Shipping Info">Shipping Info</option>
+                    </select>
+                    <input type="submit" value="Submit" />
+                </label>
+            </form>
+
+            </div>
+
+        )
+    }
+}
 }
 //<button id="checkout" onClick={this.receiveForms}>{this.state.text}</button>
 ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,8 +1,17 @@
 const express= require('express')
 const app = express();
 const PORT = 3000;
-
+app.use(express.urlencoded())
 app.use(express.static('public'))
+
+app.post('/api/client',((req, res)=>{
+    
+    console.log('Req: ',req.body.name)
+    console.log('Email: ',req.body.email)
+    console.log('Password: ',req.body.password)}
+))
+
+
 
 app.listen(PORT,()=> console.log(`Listening on port: ${PORT}`));
 
