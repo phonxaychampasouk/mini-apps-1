3984cc970ec167de632cd06ee46773feef973b61
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-04 16:29:12 -0800

    Figuring out to use form options and implementing conditional rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,24 +2,67 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-                text : "Checkout"
+                text : "Checkout",
+                value : "Account info",
+                page: "Checkout"
         }
         this.receiveForms = this.receiveForms.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.distributeForm = this.distributeForm.bind(this);
     }
 
 
-receiveForms(){
-    alert('Test worked')
-
+receiveForms() {
+    this.setState({
+      page: 'Payment Processing --->'
+    })
 }
+handleChange(event) {
+        console.log('value: ',event.target.value)
+        console.log(event.target.value)
+        this.setState({value: event.target.value});
+      }
+distributeForm() {
+    event.preventDefault()
+    FormAccountInfo();
+    console.log(event.target)
+   
+    alert('this test works')
+}
+
+FormAccountInfo = () => {
+return (<form>
+<input type="submit"></input>
+</form>)
+}
+FormProcessPayment = () => {
+<form>
+<input type="submit"></input>
+</form>
+}
+FormShippingInfo = () => {
+<form>
+<input type="submit"></input>
+</form>
+}
+
     render() {
-    return (<button id="checkout" onClick={this.receiveForms}>{this.state.text}</button>
-         
-        // <form id="formData" method="POST" action="/uploadjson">
-        //             <input type="text" name="jsonText">
-        //             <button>Submit</button>
-        //         </form>;
+    return (
+    <div> <form onSubmit={this.receiveForms}>
+            <label> {this.state.text}:
+                <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="Process Payment">Process Payment</option>
+                    <option value="Account Info">Account Info</option>
+                    <option value="Shipping Info">Shipping Info</option>
+                </select>
+                <input type="submit" value="Submit" onClick={this.distributeForm}  />
+            </label>
+            
+            </form>
+            
+    </div>
+            
          )}
 }
-
+//<button id="checkout" onClick={this.receiveForms}>{this.state.text}</button>
 ReactDOM.render(<App />, document.getElementById('app'));
